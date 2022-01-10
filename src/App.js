413ec2388c02a12ea9116796e554442cd31c1c36
Rahul <rahuldutta.rd11@gmail.com>
2022-01-10 21:46:53 +0530

    Dummy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/pages/main/Main";
-import Booking from "./components/pages/book/Book"
+import Booking from "./components/pages/book/Book";
+import About from "./components/aboutus/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/booking" element={<Booking/>}></Route>
+        <Route path="/booking" element={<Booking />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </BrowserRouter>
   );
