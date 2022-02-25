aecb33c1b5a51c2f1fdbeb0951f71bb5c9db2cf1
Rahul <rahuldutta.rd11@gmail.com>
2022-02-25 13:21:23 +0530

    bot

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 import Main from "./components/pages/main/Main";
 import Booking from "./components/pages/book/Book";
 import About from "./components/aboutus/About";
@@ -12,8 +13,11 @@ function App() {
         <Route path="/booking" element={<Booking />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/code" element={<Code />}></Route>
-
       </Routes>
+      <MessengerCustomerChat
+    pageId="109790721641423"
+    appId=" 370066444957420"
+  />,
     </BrowserRouter>
   );
 }
