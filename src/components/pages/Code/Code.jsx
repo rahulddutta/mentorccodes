d196f4fc2949574e08b5e8ab6866d4d6f2ce810b
rahulddutta <rahuldutta.rd11@gmail.com>
2022-06-26 14:59:33 +0530

    fullScreen

--- src/components/pages/Code/Code.jsx
+++ src/components/pages/Code/Code.jsx
@@ -1,14 +1,41 @@
 import React, { useState, useEffect } from "react";
 import "./code.css";
 import Editor from "../../Editor/Editor";
-import UseLocalStorage from "../../hooks/UseLocalStorage"
+import UseLocalStorage from "../../hooks/UseLocalStorage";
+import FullscreenIcon from "@material-ui/icons/Fullscreen";
+import { makeStyles } from "@material-ui/core";
+import FullscreenExitIcon from "@material-ui/icons/FullscreenExit";
 
+const useStyles = makeStyles(() => ({
+  halfPane: {
+    //display: "flex",
+    height: "50vh",
+    position: "relative",
+  },
+
+  fullPane: {
+    // display: "flex",
+    height: "100vh",
+  },
+  codeWriter: {
+    display: "none",
+  },
+  fullIcon: {
+    zIndex: 1000,
+    position: "absolute",
+    right:0
+  },
+}));
 
 export default function Code() {
-  const [html, setHtml] = UseLocalStorage('html',"");
-  const [css, setCss] =UseLocalStorage('css',"");;
-  const [js, setJs] =UseLocalStorage('js',"");
+  const classes = useStyles();
+  const [html, setHtml] = UseLocalStorage("html", "");
+  const [css, setCss] = UseLocalStorage("css", "");
+  const [js, setJs] = UseLocalStorage("js", "");
   const [srcDoc, setSrcDoc] = useState("");
+  const [fullScreen, setFullScreen] = useState(false);
+
+  console.log("fullScreen", fullScreen);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -26,27 +53,36 @@ export default function Code() {
 
   return (
     <>
-      <div className="pane top-pane">
-        <Editor
-          language="xml"
-          displayName="HTML"
-          value={html}
-          onChange={setHtml}
-        />
-        <Editor
-          language="css"
-          displayName="CSS"
-          value={css}
-          onChange={setCss}
-        />
-        <Editor
-          language="javascript"
-          displayName="JS"
-          value={js}
-          onChange={setJs}
-        />
+      <div className={fullScreen ? classes.codeWriter : null}>
+        <div className="pane top-pane">
+          <Editor
+            language="xml"
+            displayName="HTML"
+            value={html}
+            onChange={setHtml}
+          />
+          <Editor
+            language="css"
+            displayName="CSS"
+            value={css}
+            onChange={setCss}
+          />
+          <Editor
+            language="javascript"
+            displayName="JS"
+            value={js}
+            onChange={setJs}
+          />
+        </div>
       </div>
-      <div className="pane">
+      <div className={fullScreen ? classes.fullPane : classes.halfPane}>
+        <div className={classes.fullIcon}>
+          {fullScreen ? (
+            <FullscreenExitIcon onClick={() => setFullScreen(false)} />
+          ) : (
+            <FullscreenIcon onClick={() => setFullScreen(true)} />
+          )}
+        </div>
         <iframe
           srcDoc={srcDoc}
           title="output"
